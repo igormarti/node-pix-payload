5cebf9fdb9c5b2a8bc959fbd57ea9edd45823d09
igorrocha <igorrocha@msigroup.com.br>
2021-03-26 10:12:21 -0300

    modify location

--- src/test.js
+++ src/test.js
@@ -1,7 +1,6 @@
 const Payload = require('./');
 
 const location = 'qrpix.seupsp.com.br/qr/v2/c-8c7193b-34cc6180be87850a-89193060-9705a6b8';
-
 const payload = Payload.setMerchantName('IJ Solutions')
                 .setMerchantCity('VICOSA')
                 .setAmount('113.00')
